a2df1dcdd92591a9a8b29607eb103c5f6d5148ca
TofSas22 <tsasman13@gmail.com>
2024-07-13 22:34:43 +0200

    Reviews-section

--- js/custom.js
+++ js/custom.js
@@ -22,20 +22,16 @@ function displayReviews(data) {
     for (let i = 0; i < data.length; i++) {
         const review = data[i];
         output += `
-            <div class="card mb-4 box-shadow">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">${review.restaurant_name}</h4>
-                </div>
-                <div class="card-body">
-                    <img src="${review.restaurant_image.image_url}" class="card-img-top" alt="${review.restaurant_image.description}">
-                    <p class="card-text">${review.rating}</p>
-                    <p class="card-text">${review.review_short_text}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-lg btn-link"><i class="far fa-heart"></i></button>
-                            <button type="button" class="btn btn-lg btn-link"><i class="far fa-comment"></i></button>
-                            <button type="button" class="btn btn-lg btn-link"><i class="far fa-solid fa-retweet"></i></button>
-                        </div>
+            <div class="col">
+                <div class="card h-100" style="width:400px">
+                    <div class="card-img-container">
+                        <img class="card-img-top img-fluid" src="${review.restaurant_image.image_url}" alt="${review.restaurant_image.description}">
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title">${review.restaurant_name}</h4>
+                        <p class="card-text">${review.rating}⭐️</p>
+                        <p class="card-text">${review.review_short_text}</p>
+                        <a href="#" class="btn btn-primary">Full Review</a>
                         <small class="text-muted">${review.review_date}</small>
                     </div>
                 </div>
